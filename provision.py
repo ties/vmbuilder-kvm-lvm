b0758f26ed57e5034050d32458935568a8abe8df
Ties de Kock <ties@tiesdekock.nl>
2014-12-22 18:07:48 +0100

    WIP

--- provision.py
+++ provision.py
@@ -1,7 +1,27 @@
 import os
+import hashlib
+import binascii
 
 hostname = "pm-vm"
-mac = '52:54:00:1C:D7:5E'
+
+
+#
+# Random data for mac:
+#
+def random_mac():
+    mac_seed = os.urandom(512)
+    m = hashlib.sha256()
+    m.update(mac_seed)
+
+    # 6 digets in pairs of 2
+    mac_digest = bytearray(m.digest())
+
+    return '00:50:56:{:02x}:{:02x}:{:02x}'.format(
+            *list(mac_digest[:4]))
+
+
+
+mac = random_mac()
 #
 # Generate vmbuilder config:
 #
@@ -13,7 +33,7 @@ with open('vmbuilder.cfg.template', 'r') as f:
     # Add full path
     cwd = os.getcwd()
 
-    vmbuilder_config.format(cwd=cwd)
+    vmbuilder_config = vmbuilder_config.format(cwd=cwd, mac=mac)
 
     with open(vmbuilder_cfgfile, 'w') as cfg_out:
         cfg_out.write(vmbuilder_config)
@@ -23,5 +43,5 @@ with open('vmbuilder.cfg.template', 'r') as f:
 #
 print("Provision the vm with:")
 print("> ubuntu-vm-builder kvm ubuntu -c ./{cfg} --hostname={host}".format(
-    vmbuilder_cfgfile, hostname))
+    cfg=vmbuilder_cfgfile, host=hostname))
 
