6a3376b2afec272ca67a1bc5f352379e1b76b025
Ties de Kock <git@tiesdekock.nl>
2014-12-22 22:32:48 +0100

    Some more options :)

--- provision.py
+++ provision.py
@@ -18,8 +18,7 @@ def random_mac():
 
 
 
-def generate_config(hostname, cfg_file):
-    mac = random_mac()
+def generate_config(cfg_file, args):
     #
     # Generate vmbuilder config:
     #
@@ -30,7 +29,8 @@ def generate_config(hostname, cfg_file):
         # Add full path
         cwd = os.getcwd()
 
-        vmbuilder_config = vmbuilder_config.format(cwd=cwd, mac=mac)
+        vmbuilder_config = vmbuilder_config.format(cwd=cwd, mac=args.mac,
+						   mem=args.mem)
 
         with open(vmbuilder_cfgfile, 'w') as cfg_out:
             cfg_out.write(vmbuilder_config)
@@ -45,22 +45,34 @@ def which(fn):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='A ubuntu-vm-builder wrapper')
     parser.add_argument('hostname', help='The VM hostname')
+    parser.add_argument('--mac', help="The mac address", default=random_mac())
+    parser.add_argument('--mem', help="Amount of memory (MB)", default=1024)
     parser.add_argument('--really_run', action='store_true',
         help='Really call the ubuntu-vm-builder')
 
     args = parser.parse_args()
 
-    vmbuilder_cfgfile = "vmbuilder_{}.cfg".format(args.hostname)
+    if not os.path.isdir('templated_configs'):
+	os.mkdir('templated_configs')
+
+    image_dir = "images/{}".format(args.hostname)
+
+    if os.path.isdir(image_dir):
+	print("The directory '{}' should not exist! - exiting.".format(image_dir))
+	exit()
+
+    vmbuilder_cfgfile = "templated_configs/vmbuilder_{}.cfg".format(args.hostname)
     # Generate the config
-    generate_config(args.hostname, vmbuilder_cfgfile)
+    generate_config(vmbuilder_cfgfile, args)
 
 
     #
     # Print the vmbuilder command:
     #
     print("Provision the vm by calling:")
-    execv_args = ['kvm', 'ubuntu', '-c',
+    execv_args = ['ubuntu-vm-builder', 'kvm', 'ubuntu', '-c',
              './{cfg}'.format(cfg=vmbuilder_cfgfile), 
+	     '-d', image_dir,
              '--hostname={host}'.format(host=args.hostname)]
 
     print("> ubuntu-vm-builder {}".format(" ".join(execv_args)))
