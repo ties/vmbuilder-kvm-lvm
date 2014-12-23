46b7994384cd2f8177d88424e18d62c6abc6ca68
Ties de Kock <git@tiesdekock.nl>
2014-12-23 13:24:01 +0100

    Auto convert qcow2 -> LVM

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # Libvirt/KVM provisioning tools
+
+## Dependencies:
+  * python-parted (apt)
+  * python-lvm (compile, needs liblvm2-dev)
+
 ```
 python provision.py [hostname] --mem [mem] --mac [mac] --really_run
+qemu-img convert [qcow2] -O raw /dev/[vg]/[lv]
+modprobe nbd max_part=16
+qemu-nbd -c /dev/nbd[number] [image]
+parted /dev/[nbdname] unit MB print
+
 lvcreate [vgroup] -n [volume] -L [size]
 
-modprobe nbd max_part=16
-qemu-nbd -c /dev/nbd0 images/[hostname]/[...].qcow2
-partprobe /dev/nbd0
-dd if=/dev/nbd0 of=/dev/[vgroup]/[volume] bs=1M
 vim /etc/libvirt/qemu/[hostname.xml]
 ```
 
--- provision.py
+++ provision.py
@@ -1,6 +1,17 @@
 import os
 import hashlib
 import argparse
+import math
+import glob
+import subprocess
+
+import lvm
+import parted.device
+import logging
+
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
 
 #
 # Random data for mac:
@@ -30,7 +41,7 @@ def generate_config(cfg_file, args):
         cwd = os.getcwd()
 
         vmbuilder_config = vmbuilder_config.format(cwd=cwd, mac=args.mac,
-						   mem=args.mem)
+                           mem=args.mem)
 
         with open(vmbuilder_cfgfile, 'w') as cfg_out:
             cfg_out.write(vmbuilder_config)
@@ -45,21 +56,22 @@ def which(fn):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='A ubuntu-vm-builder wrapper')
     parser.add_argument('hostname', help='The VM hostname')
+    parser.add_argument('--lvm_vg', help='The LVM volume group to copy to')
     parser.add_argument('--mac', help="The mac address", default=random_mac())
     parser.add_argument('--mem', help="Amount of memory (MB)", default=1024)
     parser.add_argument('--really_run', action='store_true',
-        help='Really call the ubuntu-vm-builder')
+                        help='Really call the ubuntu-vm-builder')
 
     args = parser.parse_args()
 
     if not os.path.isdir('templated_configs'):
-	os.mkdir('templated_configs')
+        os.mkdir('templated_configs')
 
     image_dir = "images/{}".format(args.hostname)
 
     if os.path.isdir(image_dir):
-	print("The directory '{}' should not exist! - exiting.".format(image_dir))
-	exit()
+        print("The directory '{}' should not exist! - exiting.".format(image_dir))
+        exit()
 
     vmbuilder_cfgfile = "templated_configs/vmbuilder_{}.cfg".format(args.hostname)
     # Generate the config
@@ -71,12 +83,80 @@ if __name__ == "__main__":
     #
     cwd = os.getcwd()
     print("Provision the vm by calling:")
-    execv_args = ['ubuntu-vm-builder', 'kvm', 'ubuntu', '-c',
-             './{cfg}'.format(cfg=vmbuilder_cfgfile), 
-	     '-d', image_dir, '--part={cwd}/vmbuilder.partition'.format(cwd=cwd),
-             '--hostname={host}'.format(host=args.hostname)]
+    execv_args = [which('ubuntu-vm-builder'), 'kvm', 'ubuntu', '-c',
+                  './{cfg}'.format(cfg=vmbuilder_cfgfile), 
+                  '-d', image_dir, '--part={cwd}/vmbuilder.partition'.format(cwd=cwd),
+                  '--hostname={host}'.format(host=args.hostname)]
 
     print("> ubuntu-vm-builder {}".format(" ".join(execv_args)))
     
     if args.really_run:
-        os.execv(which('ubuntu-vm-builder'), execv_args)
+        subprocess.check_call(execv_args)
+    
+    if args.lvm_vg:
+        # find the qcow images:
+        for path, _, filenames in os.walk(image_dir):
+            for idx, qcow in enumerate([os.path.join(path, filename) for filename in filenames]):
+                target_lv = "disk{}".format(idx)
+                
+                log.info("{} -> {}/{}".format(qcow, args.lvm_vg, target_lv))
+                if args.really_run:
+                    qcow_to_lvm(qcow, args.lvm_vg, target_lv)
+
+# Create LVM volume of size (in bytes):
+def lvm_vg_create(vg_name, lv_name, size):
+    vg = lvm.vgOpen(vg_name, 'w')
+
+    lv = vg.createLvLinear(lv_name, size)
+    vg.close()
+    return '/dev/{vg}/{lv}'.format(vg=vg_name, lv=lv_name)
+
+"""
+Get disk size (in bytes)
+"""
+def disk_size(device_name):
+    dev = parted.device.Device(device_name)
+    return int(dev.getSize(unit="B"))
+    
+
+def qcow_to_lvm(qcow, vg_name, lv_name):
+    assert os.path.isfile(qcow)
+
+    # build full path for qcow:
+    qcow = os.path.abspath(qcow)
+
+    qemu_nbd_bin = which('qemu-nbd')
+    log.info('qemu-nbd: {}'.format(qemu_nbd_bin))
+
+    # find free nbd device:
+    nbd_mountpoint = None
+    for idx in range(16):
+        nbd = "/dev/nbd{}".format(idx)
+        if not os.path.ismount(nbd):
+            nbd_mountpoint = os.path.join("/dev", nbd)
+            break
+    
+    assert nbd_mountpoint
+    log.info("nbd mountpoint: {}".format(nbd_mountpoint))
+
+    # Mount:
+    subprocess.check_call([qemu_nbd_bin, '-c', nbd_mountpoint, qcow])
+    log.info("mounted {} on {}".format(qcow, nbd_mountpoint))
+    
+    # Get size:
+    nbd_size = disk_size(nbd_mountpoint)
+    log.info("qcow: {} bytes".format(nbd_size))
+
+    lv_device = lvm_vg_create(vg_name, lv_name, nbd_size)
+    log.info("created lv: {}".format(lv_device)) 
+
+    # Unmount nbd
+    subprocess.check_call([qemu_nbd_bin, '-d', nbd_mountpoint])
+    assert not os.path.ismount(nbd_mountpoint)
+
+    # Copy qcow -> LVM
+    assert os.path.exists(lv_device)
+    
+    log.info("copying {} -> {}".format(qcow, lv_device))
+    subprocess.check_call([which('qemu-img'), 'convert', qcow, '-O', 'raw', lv_device])
+
